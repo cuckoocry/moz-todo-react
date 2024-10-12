af1797f32ba2695e9eb8e3c7b55fa18f2b31d0a6
jiang <948742327@qq.com>
2024-10-12 17:46:08 +0800

    编辑和切换任务列表

--- src/App.js
+++ src/App.js
@@ -5,12 +5,53 @@ import FilterButton from "./components/FilterButton";
 import { useState } from "react";
 import { nanoid } from "nanoid";
 
+const FILTER_MAP = {
+  All: () => true,
+  Active: (task) => !task.completed,
+  Completed: (task) => task.completed,
+};
+const FILTER_NAMES = Object.keys(FILTER_MAP);
+
+
+
 
 // App 方法返回一个 JSX 表达式，这个表达式定义了浏览器最终要渲染的 DOM。
 // props  参数
 function App(props) {
 
   const [tasks, setTasks] = useState(props.tasks);
+
+  const [filter, setFilter] = useState("All");
+
+  const filterList = FILTER_NAMES.map((name) => (
+    <FilterButton
+      key={name}
+      name={name}
+      isPressed={name === filter}
+      setFilter={setFilter}
+    />
+  ));
+  
+   /**
+   * 任务列表
+   */
+   const taskList = tasks
+   .filter(FILTER_MAP[filter])
+   .map((task) => (
+     <Todo
+       id={task.id}
+       name={task.name}
+       completed={task.completed}
+       key={task.id}
+       toggleTaskCompleted={toggleTaskCompleted}
+       deleteTask={deleteTask}
+       editTask={editTask}
+     />
+   ));
+ 
+
+
+     
 
   /**
    * form 添加任务 将任务添加到任务列表
@@ -21,20 +62,7 @@ function App(props) {
     setTasks([...tasks, newTask]);
   }
   
-  /**
-   * 任务列表
-   */
-  const taskList = tasks?.map((task) => (
-    <Todo
-      id={task.id}
-      name={task.name}
-      completed={task.completed}
-      key={task.id}
-      toggleTaskCompleted={toggleTaskCompleted}
-      deleteTask={deleteTask}
-    />
-  ));
-  
+
   // 任务个数的展示
   const tasksNoun = taskList.length !== 1 ? "tasks" : "task";
   const headingText = `${taskList.length} ${tasksNoun} remaining`;
@@ -66,6 +94,22 @@ function App(props) {
     setTasks(remainingTasks);
   }
   
+  function editTask(id, newName) {
+    const editedTaskList = tasks.map((task) => {
+      // if this task has the same ID as the edited task
+      if (id === task.id) {
+        // Copy the task and update its name
+        return { ...task, name: newName };
+      }
+      // Return the original task if it's not the edited task
+      return task;
+    });
+    setTasks(editedTaskList);
+  }
+  
+
+  
+    
   return (
     <div className="todoapp stack-large">
       <h1>TodoMatic</h1>
@@ -73,9 +117,8 @@ function App(props) {
       <Form addTask={addTask} />
 
       <div className="filters btn-group stack-exception">
-        <FilterButton />
-        <FilterButton />
-        <FilterButton />
+      {filterList}
+
       </div>
       <h2 id="list-heading">{headingText}</h2>
       <ul
