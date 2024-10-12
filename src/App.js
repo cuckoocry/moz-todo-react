91416587079dc8661645af2228254eb0ed981925
jiang <948742327@qq.com>
2024-10-12 16:18:15 +0800

    代办任务的新增和删除

--- src/App.js
+++ src/App.js
@@ -2,31 +2,82 @@ import './App.css';
 import Todo from "./components/Todo";
 import Form from "./components/Form";
 import FilterButton from "./components/FilterButton";
+import { useState } from "react";
+import { nanoid } from "nanoid";
+
 
 // App 方法返回一个 JSX 表达式，这个表达式定义了浏览器最终要渲染的 DOM。
 // props  参数
 function App(props) {
 
-  const taskList = props.tasks.map((task) => (
+  const [tasks, setTasks] = useState(props.tasks);
+
+  /**
+   * form 添加任务 将任务添加到任务列表
+   * @param {*} name 
+   */
+  function addTask(name) {
+    const newTask = { id: `todo-${nanoid()}`, name, completed: false };
+    setTasks([...tasks, newTask]);
+  }
+  
+  /**
+   * 任务列表
+   */
+  const taskList = tasks?.map((task) => (
     <Todo
       id={task.id}
       name={task.name}
       completed={task.completed}
       key={task.id}
+      toggleTaskCompleted={toggleTaskCompleted}
+      deleteTask={deleteTask}
     />
   ));
   
+  // 任务个数的展示
+  const tasksNoun = taskList.length !== 1 ? "tasks" : "task";
+  const headingText = `${taskList.length} ${tasksNoun} remaining`;
+  
+  /**
+   *  
+   * 代办任务列表多选框
+   * @param {*} id 
+   */
+  function toggleTaskCompleted(id) {
+    const updatedTasks = tasks.map((task) => {
+      // if this task has the same ID as the edited task
+      if (id === task.id) {
+        // use object spread to make a new object
+        // whose `completed` prop has been inverted
+        return { ...task, completed: !task.completed };
+      }
+      return task;
+    });
+    setTasks(updatedTasks);
+  }
+  
+  /**
+   * 删除代办任务
+   * @param {*} id 
+   */
+  function deleteTask(id) {
+    const remainingTasks = tasks.filter((task) => id !== task.id);
+    setTasks(remainingTasks);
+  }
   
   return (
     <div className="todoapp stack-large">
       <h1>TodoMatic</h1>
-      <Form />
+
+      <Form addTask={addTask} />
+
       <div className="filters btn-group stack-exception">
         <FilterButton />
         <FilterButton />
         <FilterButton />
       </div>
-      <h2 id="list-heading">3 tasks remaining</h2>
+      <h2 id="list-heading">{headingText}</h2>
       <ul
         role="list"
         className="todo-list stack-large stack-exception"
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,8 +1,26 @@
+import { useState } from "react";
+
 import React from "react";
 
 function Form(props) {
+
+    // name 是一个 state setName 是对应的 setter 函数。
+    const [name, setName] = useState("");
+
+    function handleChange(event) {
+        // event.target.value 键盘的输入值
+        setName(event.target.value);
+    }
+
+
+    function handleSubmit(event) {
+    event.preventDefault();
+    props.addTask(name);
+    setName("");
+    }
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <h2 className="label-wrapper">
         <label htmlFor="new-todo-input" className="label__lg">
           What needs to be done?
@@ -14,6 +32,8 @@ function Form(props) {
         className="input input__lg"
         name="text"
         autoComplete="off"
+        value={name}
+        onChange={handleChange}
       />
       <button type="submit" className="btn btn__primary btn__lg">
         Add
